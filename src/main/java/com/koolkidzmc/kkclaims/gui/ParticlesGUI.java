038b24296d55c686ccde98bbc0a91fab7f746e59
Rodney_RWR <rodney.rwr.yt@gmail.com>
2023-11-06 21:45:11 +1100

    Push
    Signed-off-by: Rodney_RWR <rodney.rwr.yt@gmail.com>

--- src/main/java/com/koolkidzmc/kkclaims/gui/ParticlesGUI.java
+++ src/main/java/com/koolkidzmc/kkclaims/gui/ParticlesGUI.java
@@ -5,9 +5,13 @@ import com.koolkidzmc.kkclaims.claims.ClaimManager;
 import com.koolkidzmc.kkclaims.utils.ColorAPI;
 import com.koolkidzmc.kkclaims.utils.FastInv;
 import com.koolkidzmc.kkclaims.utils.ItemBuilder;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public class ParticlesGUI extends FastInv {
     private boolean preventClose = false;
@@ -20,7 +24,13 @@ public class ParticlesGUI extends FastInv {
         int index = 0;
         for (Particle particle : Particle.values()) {
             if (index == 45) return;
-            setItem(index, new ItemBuilder(Material.FIREWORK_ROCKET).name(ColorAPI.formatString("&f" + particle.name())).build(),
+            ItemStack item = new ItemStack(Material.FIREWORK_STAR, 1);
+            FireworkMeta fm = (FireworkMeta) item.getItemMeta();
+            fm.addEffect(FireworkEffect.builder()
+                    .withColor(Color.LIME)
+                    .build());
+            item.setItemMeta(fm);
+            setItem(index, new ItemBuilder(item).name(ColorAPI.formatString("&f" + particle.name())).build(),
                     e -> {
                         claims.setClaimBorder(player.getChunk(), particle);
                     });
